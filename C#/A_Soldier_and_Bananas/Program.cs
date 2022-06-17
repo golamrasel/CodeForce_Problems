7f11259221a75dad963b1fa84f1f8eefd8b3b92d
Golam Rasel <grasel.ctg@gmail.com>
2022-06-17 23:34:58 +0600

    Two problem solved

---
+++ C#/A_Soldier_and_Bananas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace A_Soldier_and_Bananas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+            int k, n, w,numberOfBananas = 0, costOfBananas = 0, borrowMoney = 0, output = 0; ;
+            input = Console.ReadLine().Split(' ');
+            k = int.Parse(input[0]); // Cost of first banana.
+            n = int.Parse(input[1]); // Has Dollars
+            w = int.Parse(input[2]); // Want to bananas.
+
+            for (int i = 1; i <= w; i++)
+            {
+                numberOfBananas += i;
+            }
+            costOfBananas = k * numberOfBananas;
+            borrowMoney = costOfBananas - n;
+            output = borrowMoney > 0 ? borrowMoney : output;
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ C#/An_Elephant/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace An_Elephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            var output = 0;
+            while(input > 0)
+            {
+                input -= 5;
+                output += 1;
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
