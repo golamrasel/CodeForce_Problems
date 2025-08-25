43f9bac1182d0d1ae6e1cfa13a099f451d1ec8ef
Golam Rasel <grasel.ctg@gmail.com>
2025-08-25 14:00:27 +0600

    228A Problem solved

---
+++ C#/Is_your_horseshoe_on_the_other_hoof_228A/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Is_your_horseshoe_on_the_other_hoof_228A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+           var dis = input.Distinct().Count();
+           var output = 4 - dis;
+           Console.WriteLine(output);
+        }
+
+    }
+}
