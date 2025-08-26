72500dc6a92f0fee77a5542127b281b4e6932fbb
Golam Rasel <grasel.ctg@gmail.com>
2025-08-26 18:54:02 +0600

    two proble solved

---
+++ C#/A_Hulk_705A/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace A_Hulk_705A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string result = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 1)
+                    result += "I hate";
+                else
+                    result += "I love";
+
+                if (i != n)
+                    result += " that ";
+            }
+
+            result += " it";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Ultra_Fast_Mathematician_61A/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ultra_Fast_Mathematician_61A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineOne = Console.ReadLine();
+            string lineTwo = Console.ReadLine();
+
+            string output = string.Empty;
+
+            for (int i = 0; i < lineOne.Length; i++)
+            {
+                if (lineOne[i] != lineTwo[i])
+                {
+                    output = output + "1";
+                }
+                else
+                {
+                    output = output + "0";
+                }
+            }
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
