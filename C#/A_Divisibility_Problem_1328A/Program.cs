2aaf3df7c42f8f2efcf1e7378377b954449028ea
Golam Rasel <grasel.ctg@gmail.com>
2025-08-26 19:17:39 +0600

    two problem solved

---
+++ C#/A_Divisibility_Problem_1328A/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace A_Divisibility_Problem_1328A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                int a = int.Parse(inputs[0]);
+                int b = int.Parse(inputs[1]);
+                if (a % b == 0)
+                    Console.WriteLine("0");
+                else
+                    Console.WriteLine(b - (a % b));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/A_Pangram_520A/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace A_Pangram_520A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine().ToLower();
+
+            bool isPangram = input
+                .Where(char.IsLetter)
+                .Distinct()
+                .Count() == 26;
+
+            Console.WriteLine(isPangram ? "YES" : "NO");
+            Console.ReadLine();
+        }
+    }
+}
