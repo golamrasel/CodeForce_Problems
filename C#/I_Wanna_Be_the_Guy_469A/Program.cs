737546f024daf1235bb719b56613a324966b1d62
Golam Rasel <grasel.ctg@gmail.com>
2025-08-26 21:51:39 +0600

    One Problem solved

---
+++ C#/I_Wanna_Be_the_Guy_469A/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace I_Wanna_Be_the_Guy_469A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var xLevels = Console.ReadLine().Split().Skip(1).Select(int.Parse);
+            var yLevels = Console.ReadLine().Split().Skip(1).Select(int.Parse);
+
+            var allLevels = xLevels.Concat(yLevels).Distinct();
+
+            Console.WriteLine(allLevels.Count() == n ? "I become the guy." : "Oh, my keyboard!");
+        }
+    }
+}
